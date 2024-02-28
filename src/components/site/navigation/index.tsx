4bdbc83fc2b646477ccce584b0192176f8473db3
Jeff Jiang <jeff.jiang13@gmail.com>
2024-02-28 17:05:07 -0500

    readme

--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -30,12 +30,12 @@ const Navigation = ({ user }: Props) => {
         </ul>
       </nav>
       <aside className="flex gap-2 items-center">
-        <Link
-          href={'/agency'}
-          className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80"
-        >
-          Login
-        </Link>
+        {/* Conditional rendering based on user's login status */}
+        {user === null && (
+          <Link href={'/agency'} className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80">
+            Login
+          </Link>
+        )}
         <UserButton />
         <ModeToggle />
       </aside>
