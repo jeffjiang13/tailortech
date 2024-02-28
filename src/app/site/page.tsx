96cd108f2fa5be01bc344274a5ff9f9fd70a1df2
Jeff Jiang <jeff.jiang13@gmail.com>
2024-02-28 00:16:18 -0500

    readme

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -21,28 +21,23 @@ export default async function Home() {
 
   return (
     <>
-      <section className="h-full w-full md:pt-44 mt-[-70px] relative flex items-center justify-center flex-col ">
-        {/* grid */}
+<section className="h-full w-full pt-20 relative flex items-center justify-center flex-col">
+  {/* Adjusted margins and removed absolute positioning for gradient background */}
+  <p className="text-center">Run your agency, in one place</p>
+  <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
+    <h1 className="text-9xl font-bold text-center md:text-9xl">TailorTech</h1> {/* Adjusted font size for md */}
+  </div>
+  <div className="flex justify-center items-center relative mt-10"> {/* Adjusted margin */}
+    <Image
+      src={'/assets/preview.png'}
+      alt="banner image"
+      height={1200}
+      width={1200}
+      className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
+    />
+  </div>
+</section>
 
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" />
-
-        <p className="text-center">Run your agency, in one place</p>
-        <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
-          <h1 className="text-9xl font-bold text-center md:text-[300px]">
-            TailorTech
-          </h1>
-        </div>
-        <div className="flex justify-center items-center relative md:mt-[-70px]">
-          <Image
-            src={'/assets/preview.png'}
-            alt="banner image"
-            height={1200}
-            width={1200}
-            className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
-          />
-          <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
-        </div>
-      </section>
       <section className="flex justify-center items-center flex-col gap-4 md:!mt-20 mt-[-60px]">
         <h2 className="text-4xl text-center"> Choose what fits you right</h2>
         <p className="text-muted-foreground text-center">
