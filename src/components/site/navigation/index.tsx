53668c54befcd92834677b3852a007cb41fd48df
Jeff Jiang <jeff.jiang13@gmail.com>
2024-02-28 16:06:59 -0500

    readme

--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -1,12 +1,15 @@
-import { ModeToggle } from '@/components/global/mode-toggle';
-import { UserButton, useUser } from '@clerk/nextjs';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
+import { ModeToggle } from '@/components/global/mode-toggle'
+import { UserButton } from '@clerk/nextjs'
+import { User } from '@clerk/nextjs/server'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
 
-const Navigation = () => {
-  const { isSignedIn } = useUser();
+type Props = {
+  user?: null | User
+}
 
+const Navigation = ({ user }: Props) => {
   return (
     <div className="fixed top-0 right-0 left-0 p-4 flex items-center justify-between z-10">
       <aside className="flex items-center gap-2">
@@ -27,19 +30,17 @@ const Navigation = () => {
         </ul>
       </nav>
       <aside className="flex gap-2 items-center">
-        {/* Only show the Login button if the user is not signed in */}
-        {!isSignedIn && (
-          <Link href={'/agency'}>
-            <a className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80">
-              Login
-            </a>
-          </Link>
-        )}
+        <Link
+          href={'/agency'}
+          className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80"
+        >
+          Login
+        </Link>
         <UserButton />
         <ModeToggle />
       </aside>
     </div>
-  );
-};
+  )
+}
 
-export default Navigation;
+export default Navigation
