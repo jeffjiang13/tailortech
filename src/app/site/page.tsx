358fe16953db0dd87d926ecd06028fb6eeebaab7
Jeff Jiang <jeff.jiang13@gmail.com>
2024-02-29 16:22:49 -0500

    readme

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -26,7 +26,7 @@ export default async function Home() {
 
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" />
 
-        <h4 className="text-center">Run your agency, in one place</h4>
+        <h3 className="text-center">Run your agency, in one place</h3>
         <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
           <h1 className="text-9xl font-bold text-center md:text-[200px]">
             TailorTech
