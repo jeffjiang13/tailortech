7cfd88b2459032fcc09450e53c4ea3fd1d841b17
Jeff Jiang <jeff.jiang13@gmail.com>
2024-02-29 16:52:18 -0500

    readme

--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -1,19 +1,24 @@
-import { ModeToggle } from '@/components/global/mode-toggle';
-import { UserButton } from '@clerk/nextjs';
-import { User } from '@clerk/nextjs/server';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
+import { ModeToggle } from '@/components/global/mode-toggle'
+import { UserButton } from '@clerk/nextjs'
+import { User } from '@clerk/nextjs/server'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
 
 type Props = {
-  user?: null | User;
-};
+  user?: null | User
+}
 
 const Navigation = ({ user }: Props) => {
   return (
     <div className="fixed top-0 right-0 left-0 p-4 flex items-center justify-between z-10">
       <aside className="flex items-center gap-2">
-        <Image src={'/assets/code.png'} width={40} height={40} alt="logo" />
+        <Image
+          src={'/assets/code.png'}
+          width={40}
+          height={40}
+          alt="logo"
+        />
         <span className="text-xl font-bold">TailorTech.</span>
       </aside>
       <nav className="hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
@@ -26,16 +31,20 @@ const Navigation = ({ user }: Props) => {
       </nav>
       <aside className="flex gap-2 items-center">
         {/* Conditional rendering based on user's login status */}
-        {!user ? (
-          <Link href={'/agency'}><a className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80">Login</a></Link>
+        {user ? (
+          <Link href={'/agency'} className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80">
+            Dashboard
+          </Link>
         ) : (
-          <Link href={'/agency'}><a className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80">Dashboard</a></Link>
+          <Link href={'/agency'} className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80">
+            Login
+          </Link>
         )}
         <UserButton />
         <ModeToggle />
       </aside>
     </div>
-  );
-};
+  )
+}
 
-export default Navigation;
+export default Navigation
