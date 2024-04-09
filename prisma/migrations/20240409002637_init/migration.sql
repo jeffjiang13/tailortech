cd24c2465bc53a5ccc084fa0d3035a9043b57e8e
Jeff Jiang <jeff.jiang13@gmail.com>
2024-04-08 21:05:14 -0400

    new database

---
+++ prisma/migrations/20240409002637_init/migration.sql
@@ -0,0 +1,452 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('AGENCY_OWNER', 'AGENCY_ADMIN', 'SUBACCOUNT_USER', 'SUBACCOUNT_GUEST');
+
+-- CreateEnum
+CREATE TYPE "Icon" AS ENUM ('settings', 'chart', 'calendar', 'check', 'chip', 'compass', 'database', 'flag', 'home', 'info', 'link', 'lock', 'messages', 'notification', 'payment', 'power', 'receipt', 'shield', 'star', 'tune', 'videorecorder', 'wallet', 'warning', 'headphone', 'send', 'pipelines', 'person', 'category', 'contact', 'clipboardIcon');
+
+-- CreateEnum
+CREATE TYPE "TriggerTypes" AS ENUM ('CONTACT_FORM');
+
+-- CreateEnum
+CREATE TYPE "ActionType" AS ENUM ('CREATE_CONTACT');
+
+-- CreateEnum
+CREATE TYPE "InvitationStatus" AS ENUM ('ACCEPTED', 'REVOKED', 'PENDING');
+
+-- CreateEnum
+CREATE TYPE "Plan" AS ENUM ('price_1OmlclAW3q44BEc8sffDcyGj', 'price_1OmldcAW3q44BEc8jqJFzKht');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "avatarUrl" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'SUBACCOUNT_USER',
+    "agencyId" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Permissions" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "subAccountId" TEXT NOT NULL,
+    "access" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Permissions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Agency" (
+    "id" TEXT NOT NULL,
+    "connectAccountId" TEXT DEFAULT '',
+    "customerId" TEXT NOT NULL DEFAULT '',
+    "name" TEXT NOT NULL,
+    "agencyLogo" TEXT NOT NULL,
+    "companyEmail" TEXT NOT NULL,
+    "companyPhone" TEXT NOT NULL,
+    "whiteLabel" BOOLEAN NOT NULL DEFAULT true,
+    "address" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "zipCode" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "goal" INTEGER NOT NULL DEFAULT 5,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Agency_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SubAccount" (
+    "id" TEXT NOT NULL,
+    "connectAccountId" TEXT DEFAULT '',
+    "name" TEXT NOT NULL,
+    "subAccountLogo" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "companyEmail" TEXT NOT NULL,
+    "companyPhone" TEXT NOT NULL,
+    "goal" INTEGER NOT NULL DEFAULT 5,
+    "address" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "zipCode" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "agencyId" TEXT NOT NULL,
+
+    CONSTRAINT "SubAccount_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Tag" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "subAccountId" TEXT NOT NULL,
+
+    CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Pipeline" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "subAccountId" TEXT NOT NULL,
+
+    CONSTRAINT "Pipeline_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Lane" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "pipelineId" TEXT NOT NULL,
+    "order" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Lane_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Ticket" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "laneId" TEXT NOT NULL,
+    "order" INTEGER NOT NULL DEFAULT 0,
+    "value" DECIMAL(65,30),
+    "description" TEXT,
+    "customerId" TEXT,
+    "assignedUserId" TEXT,
+
+    CONSTRAINT "Ticket_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Trigger" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" "TriggerTypes" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "subAccountId" TEXT NOT NULL,
+
+    CONSTRAINT "Trigger_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Automation" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "triggerId" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "subAccountId" TEXT NOT NULL,
+
+    CONSTRAINT "Automation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AutomationInstance" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "automationId" TEXT NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "AutomationInstance_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Action" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" "ActionType" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "automationId" TEXT NOT NULL,
+    "order" INTEGER NOT NULL,
+    "laneId" TEXT NOT NULL DEFAULT '0',
+
+    CONSTRAINT "Action_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "subAccountId" TEXT NOT NULL,
+
+    CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Media" (
+    "id" TEXT NOT NULL,
+    "type" TEXT,
+    "name" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "subAccountId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Media_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Funnel" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "description" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "subDomainName" TEXT,
+    "favicon" TEXT,
+    "subAccountId" TEXT NOT NULL,
+    "liveProducts" TEXT DEFAULT '[]',
+
+    CONSTRAINT "Funnel_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ClassName" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "funnelId" TEXT NOT NULL,
+    "customData" TEXT,
+
+    CONSTRAINT "ClassName_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FunnelPage" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "pathName" TEXT NOT NULL DEFAULT '',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "visits" INTEGER NOT NULL DEFAULT 0,
+    "content" TEXT,
+    "order" INTEGER NOT NULL,
+    "previewImage" TEXT,
+    "funnelId" TEXT NOT NULL,
+
+    CONSTRAINT "FunnelPage_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AgencySidebarOption" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL DEFAULT 'Menu',
+    "link" TEXT NOT NULL DEFAULT '#',
+    "icon" "Icon" NOT NULL DEFAULT 'info',
+    "agencyId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "AgencySidebarOption_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SubAccountSidebarOption" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL DEFAULT 'Menu',
+    "link" TEXT NOT NULL DEFAULT '#',
+    "icon" "Icon" NOT NULL DEFAULT 'info',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "subAccountId" TEXT,
+
+    CONSTRAINT "SubAccountSidebarOption_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Invitation" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "agencyId" TEXT NOT NULL,
+    "status" "InvitationStatus" NOT NULL DEFAULT 'PENDING',
+    "role" "Role" NOT NULL DEFAULT 'SUBACCOUNT_USER',
+
+    CONSTRAINT "Invitation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Notification" (
+    "id" TEXT NOT NULL,
+    "notification" TEXT NOT NULL,
+    "agencyId" TEXT NOT NULL,
+    "subAccountId" TEXT,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Subscription" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "plan" "Plan",
+    "price" TEXT,
+    "active" BOOLEAN NOT NULL DEFAULT false,
+    "priceId" TEXT NOT NULL,
+    "customerId" TEXT NOT NULL,
+    "currentPeriodEndDate" TIMESTAMP(3) NOT NULL,
+    "subscritiptionId" TEXT NOT NULL,
+    "agencyId" TEXT,
+
+    CONSTRAINT "Subscription_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AddOns" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT false,
+    "priceId" TEXT NOT NULL,
+    "agencyId" TEXT,
+
+    CONSTRAINT "AddOns_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_TagToTicket" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_agencyId_idx" ON "User"("agencyId");
+
+-- CreateIndex
+CREATE INDEX "Permissions_subAccountId_idx" ON "Permissions"("subAccountId");
+
+-- CreateIndex
+CREATE INDEX "Permissions_email_idx" ON "Permissions"("email");
+
+-- CreateIndex
+CREATE INDEX "SubAccount_agencyId_idx" ON "SubAccount"("agencyId");
+
+-- CreateIndex
+CREATE INDEX "Tag_subAccountId_idx" ON "Tag"("subAccountId");
+
+-- CreateIndex
+CREATE INDEX "Pipeline_subAccountId_idx" ON "Pipeline"("subAccountId");
+
+-- CreateIndex
+CREATE INDEX "Lane_pipelineId_idx" ON "Lane"("pipelineId");
+
+-- CreateIndex
+CREATE INDEX "Ticket_laneId_idx" ON "Ticket"("laneId");
+
+-- CreateIndex
+CREATE INDEX "Ticket_customerId_idx" ON "Ticket"("customerId");
+
+-- CreateIndex
+CREATE INDEX "Ticket_assignedUserId_idx" ON "Ticket"("assignedUserId");
+
+-- CreateIndex
+CREATE INDEX "Trigger_subAccountId_idx" ON "Trigger"("subAccountId");
+
+-- CreateIndex
+CREATE INDEX "Automation_triggerId_idx" ON "Automation"("triggerId");
+
+-- CreateIndex
+CREATE INDEX "Automation_subAccountId_idx" ON "Automation"("subAccountId");
+
+-- CreateIndex
+CREATE INDEX "AutomationInstance_automationId_idx" ON "AutomationInstance"("automationId");
+
+-- CreateIndex
+CREATE INDEX "Action_automationId_idx" ON "Action"("automationId");
+
+-- CreateIndex
+CREATE INDEX "Contact_subAccountId_idx" ON "Contact"("subAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Media_link_key" ON "Media"("link");
+
+-- CreateIndex
+CREATE INDEX "Media_subAccountId_idx" ON "Media"("subAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Funnel_subDomainName_key" ON "Funnel"("subDomainName");
+
+-- CreateIndex
+CREATE INDEX "Funnel_subAccountId_idx" ON "Funnel"("subAccountId");
+
+-- CreateIndex
+CREATE INDEX "ClassName_funnelId_idx" ON "ClassName"("funnelId");
+
+-- CreateIndex
+CREATE INDEX "FunnelPage_funnelId_idx" ON "FunnelPage"("funnelId");
+
+-- CreateIndex
+CREATE INDEX "AgencySidebarOption_agencyId_idx" ON "AgencySidebarOption"("agencyId");
+
+-- CreateIndex
+CREATE INDEX "SubAccountSidebarOption_subAccountId_idx" ON "SubAccountSidebarOption"("subAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Invitation_email_key" ON "Invitation"("email");
+
+-- CreateIndex
+CREATE INDEX "Invitation_agencyId_idx" ON "Invitation"("agencyId");
+
+-- CreateIndex
+CREATE INDEX "Notification_agencyId_idx" ON "Notification"("agencyId");
+
+-- CreateIndex
+CREATE INDEX "Notification_subAccountId_idx" ON "Notification"("subAccountId");
+
+-- CreateIndex
+CREATE INDEX "Notification_userId_idx" ON "Notification"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Subscription_subscritiptionId_key" ON "Subscription"("subscritiptionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Subscription_agencyId_key" ON "Subscription"("agencyId");
+
+-- CreateIndex
+CREATE INDEX "Subscription_customerId_idx" ON "Subscription"("customerId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AddOns_priceId_key" ON "AddOns"("priceId");
+
+-- CreateIndex
+CREATE INDEX "AddOns_agencyId_idx" ON "AddOns"("agencyId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_TagToTicket_AB_unique" ON "_TagToTicket"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_TagToTicket_B_index" ON "_TagToTicket"("B");
