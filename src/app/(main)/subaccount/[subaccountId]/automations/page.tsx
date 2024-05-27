d0df29bba0f5649ecff5952b19a151d6ccb3c298
Jeff Jiang <jeff.jiang13@gmail.com>
2024-05-27 17:49:52 -0400

    add images

--- src/app/(main)/subaccount/[subaccountId]/automations/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/automations/page.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import Image from 'next/image';
 import BlurPage from '@/components/global/blur-page'
+import Link from 'next/link';
 
 const UnderConstructionPage = () => {
   return (
     <BlurPage>
         <div className="flex flex-col justify-center items-center">
             <Image
-                src="/assets/building.jpg" // Make sure to replace this with your own image path
-                alt="Under Construction"
+                src="/assets/temp-banner.png"
+                alt="banner"
                 width={600}
                 height={600}
                 className="rounded-tl-2xl rounded-2xl border-2 border-muted shadow-2xl shadow-blue-500"
             />
-            <h1 className="text-2xl font-bold text-center mt-8">{"We're Building Something Exciting!"}</h1>
-            <p className="text-center mt-4">{"Our website is currently under construction. We'll be here soon with our new awesome site, subscribe to be notified."}</p>
-            <div className="mt-8">
-                <input
-                    type="email"
-                    placeholder="Enter your email"
-                    className="p-2 border border-gray-300 rounded-md"
-                />
-                <button className="bg-blue-500 text-white p-2 ml-2 rounded-md hover:bg-blue-400">Notify Me!</button>
+            <h1 className="text-2xl font-bold text-center mt-8">{"SAAS Automation Builder - ZapFlow"}</h1>
+            <p className="text-center mt-4">{"Check out our partner for automation. Start now by visiting ZapFlow and sign up to streamline your business and enhance your workflow efficiency."}</p>
+            <div className="mt-8 font-bold mr-[4rem]">Try now → {" "}
+                <Link href="https://jj-zapflow.vercel.app/">
+                <button className="relative bg-blue-500 text-white font-semibold py-3 px-6 rounded-md hover:bg-blue-400 focus:outline-none focus:ring-4 focus:ring-blue-300 focus:ring-opacity-50 active:bg-blue-600 active:shadow-lg transform active:scale-95 transition-all duration-300 ease-in-out shadow-md hover:shadow-lg overflow-hidden">
+                    <span className="absolute inset-0 w-full h-full bg-gradient-to-r from-blue-400 via-blue-500 to-blue-400 opacity-0 group-hover:opacity-30 transition-opacity duration-300"></span>
+                    <span className="relative">ZapFlow</span>
+                </button>
+                </Link>
             </div>
         </div>
     </BlurPage>
