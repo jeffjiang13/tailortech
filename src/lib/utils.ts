1cec4f42036c79fa0e6c1633b9c0b1e2162febcf
Jeff Jiang <jeff.jiang13@gmail.com>
2024-10-22 18:57:42 -0400

    working on stripe

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,5 +9,8 @@ export function getStripeOAuthLink(
   accountType: 'agency' | 'subaccount',
   state: string
 ) {
-  return `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_STRIPE_CLIENT_ID}&scope=read_write&redirect_uri=${process.env.NEXT_PUBLIC_URL}${accountType}&state=${state}`
+  const baseUrl = process.env.NEXT_PUBLIC_URL ?? 'https://jj-tailortech.vercel.app/subaccount'; // Provide a fallback URL here
+  const redirectUri = `${baseUrl}${baseUrl.endsWith('/') ? '' : '/'}${accountType}`;
+
+  return `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_STRIPE_CLIENT_ID}&scope=read_write&redirect_uri=${redirectUri}&state=${state}`;
 }
